46b2eef1282be0f86f9c9f036853f53237c0dfbd
Talhaalpha <talha.alphasquad@gmail.com>
2023-07-17 21:42:44 +0500

    first commit

---
+++ src/Components/Content.svelte
@@ -0,0 +1,109 @@
+<script>
+import ViewProjects from '../lib/assets/ViewProjects.png';
+import HoverOverlay from '../lib/assets/Hover Overlay.png';
+import Card from '../lib/assets/Card.png'
+</script>
+
+<main class="">
+    <!-- Hw we work -->
+    <div class="bg-[#F4F6FC] flex flex-row gap-[142px] pb-[129px] ">
+<div class="pt-[128px] pl-[165px] ml-[165px] max-w-[1224px]">
+    <p class="font-sans text-[48px] font-semibold leading-[64px]">How we work</p>
+    <p class="font-sans text-[16px] font-normal leading-[28px] max-w-[405px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+    <div class="flex flex-row items-center gap-[14px]">
+        <div>
+    <p class="font-sans text-[18px] font-medium leading-[32px] text-[#2405F2]">Get in touch with us 
+        </p></div>
+        <div>
+        <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#2405F2"/>
+            </svg>
+        </div>
+        </div>
+</div>
+<!-- 4 stratgies Div -->
+<div class="mt-[128px] flex flex-col gap-[48px]">
+    <div>
+
+    
+    <!-- Stategy 1  -->
+    <div class="flex flex-row items-center justify-center">
+    <div>
+        <svg class="relative" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_126_589" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="50">
+            <rect width="50" height="50" rx="8" fill="#5C5E84"/>
+            </mask>
+            <g mask="url(#mask0_126_589)">
+            <rect x="-26" y="27" width="50" height="50" rx="8" fill="#FCD980"/>
+            <rect x="8" y="-9" width="50" height="50" rx="8" fill="#5239FA"/>
+            </g>
+            </svg>
+<p class="absolute top-[900px] left-[890px] text-white">01</p>    
+<p class="font-sans text-[32px] font-medium leading-[48px]">Strategy</p>
+<p class="font-sans text-[16px] font-normal leading-[28px] max-w-[303px]">Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam .</p>     
+    </div>
+     <!-- Stategy 2-->
+     <div>
+        <svg class="relative" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_126_589" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="50">
+            <rect width="50" height="50" rx="8" fill="#5C5E84"/>
+            </mask>
+            <g mask="url(#mask0_126_589)">
+            <rect x="-26" y="27" width="50" height="50" rx="8" fill="#FCD980"/>
+            <rect x="8" y="-9" width="50" height="50" rx="8" fill="#5239FA"/>
+            </g>
+            </svg>
+<p class="absolute top-[900px] left-[1195px] text-white">02</p>    
+<p class="font-sans text-[32px] font-medium leading-[48px]">Wireframing</p>
+<p class="font-sans text-[16px] font-normal leading-[28px] max-w-[303px]">Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam .</p>     
+    </div>
+</div>
+</div>
+<!-- Stratgey 3 -->
+    <div>
+        <svg class="relative" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_126_589" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="50" height="50">
+            <rect width="50" height="50" rx="8" fill="#5C5E84"/>
+            </mask>
+            <g mask="url(#mask0_126_589)">
+            <rect x="-26" y="27" width="50" height="50" rx="8" fill="#FCD980"/>
+            <rect x="8" y="-9" width="50" height="50" rx="8" fill="#5239FA"/>
+            </g>
+            </svg>
+        <p class="absolute top-[1103px] left-[890px] text-white">03</p>    
+        <p class="font-sans text-[32px] font-medium leading-[48px]">Design</p>
+        <p class="font-sans text-[16px] font-normal leading-[28px] max-w-[303px]">Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam .</p> 
+    </div>
+</div>
+    </div>
+    <!-- View our projects -->
+    <div class="max-w-[1280px] m-auto pt-[128px] flex flex-col">
+        <!-- inner Div -->
+        <div>
+        <div class="flex flex-row items-center justify-between">
+        <div class="font-sans text-[48px] font-semibold leading-[64px]">
+            <p>View our projects</p>
+
+        </div>
+        <div class="flex flex-row items-center gap-[16px]">
+            <a href="/" class="font-sans text-[16px] font-medium leading-[28px]">View More</a>
+            <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#282938"/>
+                </svg>
+            </div>
+           
+        </div>
+       <!-- View our projects Imaged -->
+       <div class="flex flex-row gap-[31px]">
+                <div>
+                    <img src={ViewProjects} alt=""/>     </div>
+<div>
+    <img src={HoverOverlay} alt=""/>
+    <img src="{Card}" alt=""/>
+
+</div>
+
+                </div>
+    </div>
+    </div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Content from "../Components/Content.svelte";
+import Header from "../Components/Header.svelte";
+    import "../app.css";
+</script>
+
+<Header />
+<Content />
